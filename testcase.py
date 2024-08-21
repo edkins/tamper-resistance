fb7af36b0ebc74a673c97c409a56a51d43361929
Giles Edkins <edkins@gmail.com>
2024-08-21 17:26:22 -0400

    Identified dodgy tensor possibly

--- testcase.py
+++ testcase.py
@@ -1,15 +1,8 @@
-import functools
-import os
-from typing import Callable, List, Optional, Union
-
 import torch
 from accelerate import Accelerator, FullyShardedDataParallelPlugin
-from torch.distributed.fsdp.wrap import lambda_auto_wrap_policy, always_wrap_policy
 from transformers import (
     AutoModelForCausalLM,
 )
-from transformers.models.llama.modeling_llama import LlamaDecoderLayer
-from datasets.utils import _dill
 
 def main():
     model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen1.5-0.5B-Chat")
@@ -22,15 +15,11 @@ def main():
     model(
         torch.ones((1, 1), dtype=torch.int32), #.to(accelerator.device),
         attention_mask=torch.ones((1, 1), dtype=torch.bool), #.to(accelerator.device),
-        use_cache=False
     )
-    breakpoint()
-    # for param in model.parameters():
-    #     print(param.device)
-    #     _dill.dumps(param)
-    _dill.dumps(model)
 
     breakpoint()
+    #str(model.lm_head.weight)
+    model.lm_head.weight.sum().item()
 
 if __name__ == "__main__":
     main()
